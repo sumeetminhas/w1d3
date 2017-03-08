fc26643f01c595b5162de2f493ec086d68f21bb3
Sumeet <sumeetm273@gmail.com>
2017-03-08 20:18:37 +0000

    all characters in a input string and their respective counts

---
+++ countLetters.js
@@ -0,0 +1,18 @@
+function countLetters(inputString) {
+  var outputObject = {};
+  for (var i = 0; i < inputString.length; i++) {
+    var character = inputString[i];
+
+    if (outputObject[character]) {
+      temp = outputObject[character]
+      outputObject[character] = temp + 1;
+    } else if (character === ' '){
+      break;
+    } else {
+      outputObject[character] = 1;
+    }
+  }
+  return outputObject;
+}
+
+console.log(countLetters("lighthouse in the house"));
